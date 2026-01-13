aee8fd8f99a9ebd972d42e45bd28507ec5591ccb
Ante Prkacin <ante.prkacin03@gmail.com>
2026-01-13 06:49:29 +0100

    BE ensure maxCapacity and currentLoad are non-negative values

--- central-backend/src/main/java/fer/project/central/model/BTS.java
+++ central-backend/src/main/java/fer/project/central/model/BTS.java
@@ -1,6 +1,7 @@
 package fer.project.central.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -42,10 +43,12 @@ public class BTS {
     private String status;
 
     @NotNull(message = "Max capacity is required")
+    @Min(value = 0, message = "Max capacity must be non-negative")
     @Column(name = "max_capacity")
     private Integer maxCapacity;
 
     @NotNull(message = "Current load is required")
+    @Min(value = 0, message = "Current load must be non-negative")
     @Column(name = "current_load")
     private Integer currentLoad;
 
